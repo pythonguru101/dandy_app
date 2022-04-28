31749f99de7b494d894a3177ecff5ed73f868f8f
jessanKhan <39450312+jessanKhan@users.noreply.github.com>
2022-04-28 07:22:19 +0600

    connection to device

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # dandy_app
+
+# Dandy is an Autonomous Weed Eliminating Robot
+
+Here we have Crossplatform Apps for mobile devices like Android and iOS built with React-Native. And Web Application build with ReactJs.
+
+There Are two directories currently available.
+
+# dandy-web is the web application directory
+
+and
+
+# dandyapp is the mobile application directory
+
+Both Project needs different methods to run. Here is step by step procedures mentioned to run both of the projects.
+
+# Mobile App
+
+First you need to navigate to the dandyapp directory. You can do it by `cd dandyapp` on your terminal or by opening the directory directly from your text editor.
+
+1. Open your terminal on the project directory
+2. run the command `npm install`
+3. After succesfully finising installing npm run this command for run the project on Android `npx react-native run-android` or `npx react-native run-ios` for iOS
+4. If you dont have any emulator or mobile device connected please make sure to have either of these befor running the project
+
+# Web App
+
+First You need to Navigate to the project directory by `cd dandy-web` on your terminal from the root directory the follow the steps mentioned below.
+
+1. Open your terminal on the project directory
+2. run the command `npm install`
+3. After succesfully finising installing npm run this command for run the project `npm start`
--- dandyapp/src/screen/Home/Home.js
+++ dandyapp/src/screen/Home/Home.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View ,Button,TextInput} from 'react-native'
+import { StyleSheet, Text, View, Button, TextInput, PermissionsAndroid, TouchableOpacity, Modal, Pressable, KeyboardAvoidingView } from 'react-native'
 import React from 'react'
 import WifiManager from "react-native-wifi-reborn";
 
@@ -10,19 +10,25 @@ const Home = () => {
     const [wifiStatus, setWifiStatus] = React.useState('');
     const [wifiConnected, setWifiConnected] = React.useState(false);
     const [wifiConnecting, setWifiConnecting] = React.useState(false);
-    
+    const [permission, setPermission] = React.useState('');
+    const [modalVisible, setModalVisible] = React.useState(false);
+    const [modalText, setModalText] = React.useState('');
 
-    const getWifiList = () => {
-       WifiManager.loadWifiList().then(wifiList => {
-            setWifiList(wifiList);
-        });
 
-    };
+
+
+
+
 
     const connectToWifi = () => {
         setWifiConnecting(true);
-        WifiManager.connectToProtectedSSID(ssid, password)
-            .then(wifi => console.log(wifi))
+        WifiManager.connectToProtectedSSID(ssid, password, false)
+            .then(wifi => {
+                setWifiConnecting(false);
+                setWifiConnected(true);
+                setWifiStatus(wifi.status);
+                console.log("connected", wifi)
+            })
             .catch(error => {
                 console.log(error);
                 setWifiConnecting(false);
@@ -30,50 +36,156 @@ const Home = () => {
             });
     };
 
-    const disconnectFromWifi = () => {
-        WifiManager.disconnectFromWifi()
-            .then(wifi => {
-                setWifiConnected(false);
-            })
-            .catch(error => {
-                console.log(error);
-            });
+    // const disconnectFromWifi = () => {
+    //     WifiManager.disconnectFromWifi()
+    //         .then(wifi => {
+    //             setWifiConnected(false);
+    //         })
+    //         .catch(error => {
+    //             console.log(error);
+    //         });
+    // };
+
+    const getWifiList = async () => {
+        await WifiManager.loadWifiList().then(wifiList => {
+            setWifiList(wifiList);
+        });
+
     };
 
     const getWifiStatus = () => {
-        WifiManager.connectionStatus()
-            .then(wifiStatus => {
-                setWifiStatus(wifiStatus);
-            })
-            .catch(error => {
-                console.log(error);
-            });
+        WifiManager.getCurrentWifiSSID().then(
+            ssid => {
+                console.log("Your current connected wifi SSID is " + ssid);
+                setWifiConnected(true);
+                setWifiStatus("Connected to " + ssid);
+            },
+            () => {
+                console.log("Cannot get current SSID!");
+            }
+        );
+    };
+    const getPermission = async () => {
+        try {
+            const granted = await PermissionsAndroid.request(
+                PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+                {
+                    title: 'Location permission is required for WiFi connections',
+                    message:
+                        'This app needs location permission as this is required  ' +
+                        'to scan for wifi networks.',
+                    buttonNegative: 'DENY',
+                    buttonPositive: 'ALLOW',
+                },
+            );
+
+            setPermission(granted)
+
+        }
+        catch (err) {
+            console.warn(err);
+        }
     };
 
     React.useEffect(() => {
-        getWifiList();
-        getWifiStatus();
-      
-    }, []);
+        getPermission()
+        if (permission === PermissionsAndroid.RESULTS.GRANTED) {
+            // getWifiList();
+            getWifiStatus();
+            console.log("Wifi List", wifiList);
+        } else {
+            console.log("Permission denied");
+        }
+        // console.log(permission);
+    }, [permission]);
 
-const onChangeSSID = (text) => {
-    setSsid(text);
-}
+    const onChangeSSID = (text) => {
+        setSsid(text);
+    }
 
-const onChangePassword = (text) => {
-    setPassword(text);
-}
+    const onChangePassword = (text) => {
+        setPassword(text);
+    }
 
-    
+
     return (
         <View style={styles.container}>
-            <Text>Home</Text>
-            <View style={styles.buttonStyle}>
-                <TextInput style={styles.textInputStyle} onChange={(t)=>onChangeSSID(t)} placeholder="Enter SSID" />
-                <TextInput style={styles.textInputStyle} onChange={(t)=>onChangePassword(t)} placeholder="Enter Password" />
-            <Button
-             title="Connect" onPress={() => connectToWifi()} />
-            </View>
+            <KeyboardAvoidingView >
+
+                <Modal
+                    animationType="slide"
+                    transparent={true}
+                    visible={modalVisible}
+                    onRequestClose={() => {
+                        Alert.alert("Modal has been closed.");
+                        setModalVisible(!modalVisible);
+                    }}
+                >
+                    <View style={styles.centeredView}>
+                        <View style={styles.modalView}>
+                            <Text style={styles.modalText}>{modalText}</Text>
+                            <TextInput
+                                style={styles.input}
+                                onChangeText={onChangePassword}
+                                value={password}
+                                placeholder="password"
+                                keyboardType="default"
+                            />
+                            <View style={{ flexDirection: "row", justifyContent: "space-between" }} >
+                                <Pressable
+                                    style={[styles.button, styles.buttonConnect]}
+                                    onPress={() => connectToWifi()}
+                                >
+                                    <Text style={styles.textStyle}>Connect</Text>
+                                </Pressable>
+                                <Pressable
+                                    style={[styles.button, styles.buttonCancel]}
+                                    onPress={() => setModalVisible(!modalVisible)}
+                                >
+                                    <Text style={styles.textStyle}>Cancel</Text>
+                                </Pressable>
+                            </View>
+                        </View>
+                    </View>
+                </Modal>
+
+
+                <Text>Home</Text>
+                {/* <View style={styles.buttonStyle}>
+                <TextInput style={styles.textInputStyle} onChange={(t) => onChangeSSID(t)} placeholder="Enter SSID" />
+                <TextInput style={styles.textInputStyle} onChange={(t) => onChangePassword(t)} placeholder="Enter Password" />
+                <Button title="Connect" onPress={() => connectToWifi()} />
+            </View> */}
+                <View>
+                    <Text>Status: {wifiStatus || "Not connected"}</Text>
+                </View>
+                {/* wifi list */}
+                <View>
+                    <Button title="Search" onPress={() => getWifiList()} />
+
+                    <Text>Available Devices</Text>
+                    {wifiList.length > 0 ? wifiList.map((wifi, index) => {
+
+                        console.log("mapped", wifi.SSID)
+                        return (
+                            <TouchableOpacity key={index} onPress={() => {
+                                setModalVisible(true)
+                                setModalText(wifi.SSID)
+
+                            }}>
+                                <View style={{ flexDirection: "row", backgroundColor: "#B8FFF9" }} >
+                                    <Text>{index + 1}{". "}</Text>
+                                    <Text>{wifi.SSID}{"  "}</Text>
+                                    <Text>{wifi.BSSID}</Text>
+                                </View>
+                            </TouchableOpacity>
+                        )
+
+                    }) : <Text>No Devices found</Text>}
+                </View>
+
+            </KeyboardAvoidingView>
+
         </View>
     )
 }
@@ -83,7 +195,7 @@ export default Home
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: '#9ADCFF',
         alignItems: 'center',
         // justifyContent: 'center',
     },
@@ -94,4 +206,70 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         margin: 10,
     },
+    textInputStyle: {
+        width: 150,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: 10,
+        borderColor: '#000',
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: "white",
+        borderRadius: 20,
+        padding: 35,
+        // alignItems: "center",
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
+        width: "100%",
+        height: "30%"
+
+    },
+    button: {
+        borderRadius: 20,
+        padding: 10,
+        elevation: 2,
+        width: "30%",
+    },
+    buttonCancel: {
+        backgroundColor: "#F194FF",
+
+
+    },
+    buttonConnect: {
+        backgroundColor: "#2196F3",
+
+    },
+    textStyle: {
+        color: "white",
+        fontWeight: "bold",
+        textAlign: "center"
+    },
+    modalText: {
+        marginBottom: 15,
+        textAlign: "center",
+        fontSize: 20
+    },
+    centeredView: {
+        flex: 1,
+        justifyContent: "flex-end",
+        alignItems: "center",
+        marginTop: 22,
+        width: "100%"
+    },
+    input: {
+        height: 40,
+        // width: "100%",
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+    },
+
 })
