aca4d2bda375e6bb4873f56af81c58c900f84be2
jessanKhan <39450312+jessanKhan@users.noreply.github.com>
2022-06-01 01:20:50 +0600

    Update hostname on services

--- dandyapp/src/services/services.js
+++ dandyapp/src/services/services.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const baseUrl = 'http://127.0.0.1:5000';
+const baseUrl = 'http://dandy_robot';
 
 export const setWifiCreds = (value) => {
     console.log("Set Wifi cred", value);
